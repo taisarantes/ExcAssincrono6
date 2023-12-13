7d13dee22628b587e6fd2bb496727eb31457b7e6
taisarantes <tais.reis.arantes@gmail.com>
2023-12-13 15:15:34 -0300

    correção exercício 1

--- src/AtvdAssincrona6/Exercicio1.java
+++ src/AtvdAssincrona6/Exercicio1.java
@@ -19,49 +19,59 @@ public class Exercicio1 {
 		System.out.println("3 - Retirar cliente da fila");
 		System.out.println("0 - Sair");
 		System.out.println("          *****");
-		System.out.println("Entre com a opção desejada: ");
-		opcao = leia.nextInt();
 		
-		switch(opcao) {
-		case 1:
-			System.out.println("Digite o nome: " );
-			nome = leia.nextLine();
-			filaClientes.add(nome);
+		do {
+			System.out.println("\nEntre com a opção desejada: ");
+			opcao = leia.nextInt();
+			leia.nextLine(); // para limpar o buffer e ele parar de ignorar todas as linhas do case 1
 			
-			System.out.println("Fila: ");
-			for(String cliente : filaClientes) {
-				System.out.println(cliente);
-			}
-			
-			System.out.println("CLiente adicionado!");
-			
-		case 2:
-			if(filaClientes.isEmpty()) {
-				System.out.println("A fila está vazia!");
-			} else {
-				filaClientes.remove();
-				System.out.println("Lista de clientes na fila: ");
+			switch(opcao) {
+			case 1:
+				System.out.println("Digite o nome: " );
+				nome = leia.nextLine();
+				filaClientes.add(nome);
+				
+				System.out.println("Fila: ");
 				for(String cliente : filaClientes) {
 					System.out.println(cliente);
 				}
-			}
-			
-		case 3:
-			if(filaClientes.isEmpty()) {
-				System.out.println("A fila está vazia!");
-			} else {
-				System.out.println("Lista de clientes na fila: ");
-				for(String cliente : filaClientes) {
-					System.out.println(cliente);
+				
+				System.out.println("CLiente adicionado!");
+				break;
+				
+			case 2:
+				if(filaClientes.isEmpty()) {
+					System.out.println("A fila está vazia!");
+					break;
+				} else {
+					filaClientes.remove();
+					System.out.println("Lista de clientes na fila: ");
+					for(String cliente : filaClientes) {
+						System.out.println(cliente);
+					}
+					break;
 				}
+				
+			case 3:
+				if(filaClientes.isEmpty()) {
+					System.out.println("A fila está vazia!");
+					break;
+				} else {
+					System.out.println("Lista de clientes na fila: ");
+					for(String cliente : filaClientes) {
+						System.out.println(cliente);
+					}
+					break;
+				}
+				
+			case 0:
+				System.out.println("Programa finalizado!");
+				break;
+			default:
+				System.out.println("Opção invalidada, por favor digite outro número.");
 			}
-			
-		case 0:
-			System.out.println("Programa finalizado!");
-			break;
-		default:
-			System.out.println("Opção invalidada, por favor digite outro número.");
-		}
+		} while (opcao != 0);
+		
 	}
 
 }
